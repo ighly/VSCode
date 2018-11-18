db8f13aa21cf75478d4c9dce5e604eaafa0d5482
Jessica Willis <Jessica@Jessicas-MacBook-Pro.local>
2018-11-18 01:52:11 +0000

    item menu added and menu alterationss

--- FirstGame/Assets/Scipts/menuController.cs
+++ FirstGame/Assets/Scipts/menuController.cs
@@ -4,9 +4,10 @@ public class menuController : MonoBehaviour {
 
 	private Canvas canvas;
 	private playerMovement pm;
-	public GameObject select; 
+	public GameObject select;
 	private int count = 0;
-
+	public CanvasGroup itemMenu;
+	private bool itemOn = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,24 +17,82 @@ public class menuController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyUp(KeyCode.Z))
-		{
-			canvas.enabled = !canvas.enabled;
-			pm.enabled = !pm.enabled;
-			count = 0;
-		} 
-
 		Vector3[] positionArray = new Vector3[3];
 
 		positionArray[0] = new Vector3(-141,108,0);
 		positionArray[1] = new Vector3(-141,71,0);
 		positionArray[2] = new Vector3(-141,145,0);
 
-		if (Input.GetKeyUp(KeyCode.DownArrow))
+		Vector3[] itemArray = new Vector3[12]; 
+
+		itemArray[0] = new Vector3(-25,140,0);
+		itemArray[1] = new Vector3(139,140,0);
+		itemArray[2] = new Vector3(-25,102,0);
+		itemArray[3] = new Vector3(139,102,0);
+		itemArray[4] = new Vector3(-25,63,0);
+		itemArray[5] = new Vector3(139,63,0);
+		itemArray[6] = new Vector3(-25,24,0);
+		itemArray[7] = new Vector3(139,24,0);
+		itemArray[8] = new Vector3(-25,-14,0);
+		itemArray[9] = new Vector3(139,-14,0);
+		itemArray[10] = new Vector3(-25,-51,0);
+		itemArray[11] = new Vector3(139,-51,0);
+
+		//toggle canvas, toggle player movement, move menu select to origin
+
+		if(Input.GetKeyUp(KeyCode.Z))
+		{
+			if(!canvas.enabled)
+			{
+			canvas.enabled = true;
+			pm.enabled = false;
+			count=0;
+			select.transform.localPosition = positionArray[2];
+			itemMenu.alpha=0;
+			}
+			else
+			{
+				if(canvas.enabled)
+				{
+					itemMenu.alpha=1;
+					itemOn = true;
+					select.transform.localPosition = itemArray[0];
+				}
+			}			
+		}
+
+		if (Input.GetKeyUp(KeyCode.X))
+		{
+			if(!itemOn)
+			{
+				canvas.enabled = false;
+				pm.enabled = true;
+			}
+			else {
+				if (itemOn)
+				{
+					itemMenu.alpha = 0;
+					itemOn=false;
+				}
+			}
+		}
+
+		//cycles through menu select positions
+		if (Input.GetKeyUp(KeyCode.DownArrow) && canvas.enabled)
 		{
 			select.transform.localPosition = positionArray[count];
 			count++;
 			if (count>=3)
+			{
+				count=0;
+			}
+		}
+
+		if (Input.GetKeyUp(KeyCode.UpArrow) && canvas.enabled)
+		{
+			select.transform.localPosition = positionArray[count];
+			count--;
+			if (count<=3)
 			{
 				count=0;
 			}
--- FirstGame/Assets/Scipts/playerMovement.cs
+++ FirstGame/Assets/Scipts/playerMovement.cs
@@ -7,33 +7,49 @@ public Rigidbody rb;
 public float forwardForce =2000f;
 public float sidewaysForce = 500f;
 
+public Sprite fwdsprite;
+public Sprite bcksprite;
+
+	void Start ()
+	{
+	}
+
+
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
 		rb.AddForce(0,0,forwardForce * Time.deltaTime);
 
-		if(Input.GetKey("s"))
+		if(Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
 		{
 			rb.velocity = Vector3.zero;
 			rb.AddForce(sidewaysForce * Time.deltaTime,0,0);
+			rb.transform.eulerAngles = new Vector3(0,45,0);
+			GetComponent<SpriteRenderer>().sprite = fwdsprite;
 		}
 
-		if(Input.GetKey("w"))
+		if(Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
 		{
 			rb.velocity = Vector3.zero;
 			rb.AddForce(-sidewaysForce * Time.deltaTime,0,0);
+			rb.transform.eulerAngles = new Vector3(0,225,0);
+			GetComponent<SpriteRenderer>().sprite = bcksprite;
 		}
 
-		if(Input.GetKey("a"))
+		if(Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
 		{
 			rb.velocity = Vector3.zero;
 			rb.AddForce(0,0,-sidewaysForce * Time.deltaTime);
+			rb.transform.eulerAngles = new Vector3(0,45,0);
+			GetComponent<SpriteRenderer>().sprite = bcksprite;
 		}
 
-		if(Input.GetKey("d"))
+		if(Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
 		{
 			rb.velocity = Vector3.zero;
 			rb.AddForce(0,0,sidewaysForce * Time.deltaTime);
+			rb.transform.eulerAngles = new Vector3(0,225,0);
+			GetComponent<SpriteRenderer>().sprite = fwdsprite;
 		}
 	}
 }
